9fea9838c2cea75454564fa674cf459b0566c4b1
0biglife <kimchoon42@gmail.com>
2021-09-17 17:25:56 +0900

    StyledComponentsTest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { styles } from './style';
-import { View, StatusBar} from 'react-native';
-import Shadow from './Shadow';
+import { StatusBar } from 'expo-status-bar';
+import styled, { css } from 'styled-components/native';
 
+const Container = styled.View`
+    flex: 1;
+    background-color: #e3e3e3;
+    align-items: center;
+    justify-content: center;
+`;
+
+const cssText = css`
+    font-size: 20px;
+    font-weight: 600;
+`;
+
+const StyledText = styled.Text`
+    ${cssText}
+    color: blue;
+`;
+
+const ErrorTst = styled(StyledText)` //styled 상속 받아서 쓰기
+    ${cssText}
+    color: red;
+`;
+
+const StyledButton = styled.Button`
+
+`;
+ 
 export default function App() {
     return (
-    <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Shadow />
-    </View>
+      <Container>
+          <StatusBar style="auto" />
+          <StyledText>Styled Components!</StyledText>
+          <StyledButton title="Styled" onPress={() => alert('alert!')} />
+      </Container>
     );
 }
