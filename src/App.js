1e90b72fe6cd31afaf336b73b5a9bd1d3a2844b1
0biglife <kimchoon42@gmail.com>
2021-09-17 16:34:24 +0900

    FlexBox

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import React, {useState} from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
-import MyButton from './MyButton';
+import React from 'react';
+import { styles } from './style';
+import { View, StatusBar } from 'react-native';
+// import MyButton from './MyButton';
+import Box from './Box';
 
 export default function App() {
-    const [addition, setAddition] = useState(0);
-    const [multiple, setmultiple] = useState(1);
-
     return (
     <View style={styles.container}>
-        <TextInput 
-        // onChange={event => console.log(event.nativeEvent.text)}
-        onChangeText={text => console.log(text)}
-        style={{borderWidth: 1, padding: 10, fontSize: 20}}
-        />
-        <Text style={styles.errorText}>{addition}</Text>
-        <Text style={{fontSize: 20}}>{multiple}</Text>
-        <MyButton title="Addition" onPress={() => setAddition(addition + 2)} />
-        <MyButton title="Multiple" onPress={() => setmultiple(multiple * 2)} /> 
+        <StatusBar style="auto" />
+        <Box style={{ backgroundColor: 'red', flex: 1 }} />
+        <Box style={{ backgroundColor: 'green', flex: 1 }} />
+        <Box style={{ backgroundColor: 'blue', flex: 1 }} />
     </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    errorText: {
-        backgroundColor: 'black',
-        color: 'red',
-        fontSize: 20
-    }
-});
---
+++ src/Box.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Box = ({ style }) => {
+    return <View style={[{borderWidth: 2, width: '100%'}, style]}></View>
+};
+
+export default Box;
